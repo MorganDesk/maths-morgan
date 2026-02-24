c0a3d0b3b0143e4aacdf048b74658521df002980
Faellatv <faellatv@gmail.com>
2026-02-24 09:02:35 +0100

    Update config.js
    Retour des icones devant les onglets !

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // config.js
 const NAV_MENU = [
-    { id: "6e", nom: '6ème', url: 'index.html?lvl=6e' },
-    { id: "5e", nom: '5ème', url: 'index.html?lvl=5e' },
-    { id: "4e", nom: '4ème', url: 'index.html?lvl=4e' },
-    { id: "3e", nom: '3ème', url: 'index.html?lvl=3e' },
-	{ id: "playlists", nom: 'Parcours', url: 'playlist.html' },
-    { id: "archives", nom: 'Archives', url: 'archives.html' },
-    { id: "favoris", nom: 'Favoris', url: 'favoris.html' },
-    { id: "jeux", nom: 'Jeux', url: 'jeux.html' }
+    { id: "6e", nom: '<i class="fas fa-seedling"></i> 6ème', url: 'index.html?lvl=6e' }, // Jeune pousse
+    { id: "5e", nom: '<i class="fas fa-leaf"></i> 5ème', url: 'index.html?lvl=5e' },     // Feuille
+    { id: "4e", nom: '<i class="fas fa-tree"></i> 4ème', url: 'index.html?lvl=4e' },     // Petit arbre / Arbuste
+    { id: "3e", nom: '<i class="fas fa-graduation-cap"></i> 3ème', url: 'index.html?lvl=3e' }, // Ou 'fa-tree' plus dense
+    { id: "playlists", nom: '<i class="fas fa-route"></i> Parcours', url: 'playlist.html' },
+    //{ id: "archives", nom: '<i class="fas fa-box-archive"></i> Archives', url: 'archives.html' },
+    { id: "favoris", nom: '<i class="fas fa-star"></i> Favoris', url: 'favoris.html' },
+    { id: "jeux", nom: '<i class="fas fa-gamepad"></i> Jeux', url: 'jeux.html' }
 ];
