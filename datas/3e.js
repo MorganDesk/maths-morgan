5bc224a06a8c176072f9abed2da687396f334c29
Faellatv <faellatv@gmail.com>
2026-03-13 21:37:52 +0100

    Séquence 8 9 10 3e

--- datas/3e.js
+++ datas/3e.js
@@ -1,107 +1,314 @@
 var data3e = [
     {
-        annee: "2025-2026",
-        date: "2025-09-01",
-        niveau: "3e",
-        matiere: "Fonctions",
-        titre: "Notion de fonction",
-        id: "3e-2025-2026-notion-de-fonction",
-        desc: "Apprendre à transformer un nombre : de l'antécédent à l'image via des notations et des graphiques.",
-        fichiers: [
-           { nom: "Leçon", url: "cours/25-26/3e/01 Notion de fonction/Séquence 1 _ Notion de fonction.pdf"},
-			{ nom: "Exercices", url: "cours/25-26/3e/01 Notion de fonction/Exercices - Notion de fonction.pdf"},
-			{ nom: "Correction", url: "cours/25-26/3e/01 Notion de fonction/Correction - Notion de fonction.pdf"}
+        "annee": "2025-2026",
+        "date": "2025-09-01",
+        "niveau": "3e",
+        "matiere": "Fonctions",
+        "titre": "Notion de fonction",
+        "id": "3e-2025-2026-notion-de-fonction",
+        "desc": "Apprendre à transformer un nombre : de l'antécédent à l'image via des notations et des graphiques.",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/3e/01 Notion de fonction/Séquence 1 _ Notion de fonction.pdf"
+            },
+            {
+                "nom": "Exercices",
+                "url": "cours/25-26/3e/01 Notion de fonction/Exercices - Notion de fonction.pdf"
+            },
+            {
+                "nom": "Correction",
+                "url": "cours/25-26/3e/01 Notion de fonction/Correction - Notion de fonction.pdf"
+            }
         ],
-        tags: ["fonction", "image", "antecedent", "f(x)", "courbe", "graphique", "notation", "variable", "brevet"]
+        "tags": [
+            "fonction",
+            "image",
+            "antecedent",
+            "f(x)",
+            "courbe",
+            "graphique",
+            "notation",
+            "variable",
+            "brevet"
+        ]
     },
     {
-        annee: "2025-2026",
-        date: "2025-10-05",
-        niveau: "3e",
-        matiere: "Géométrie",
-        titre: "Trigonométrie",
-        id: "3e-2025-2026-trigonometrie",
-        desc: "Le lien entre angles et longueurs dans le triangle rectangle : Cosinus, Sinus et Tangente.",
-        fichiers: [
-            { nom: "Leçon", url: "cours/25-26/3e/02 Trigonométrie/Séquence 2 _ Trigonométrie.pdf"},
-			{ nom: "Exercices", url: "cours/25-26/3e/02 Trigonométrie/Exercices - Trigonométrie.pdf"},
-			{ nom: "Correction", url: "cours/25-26/3e/02 Trigonométrie/Correction - Trigonométrie.pdf"}
+        "annee": "2025-2026",
+        "date": "2025-10-05",
+        "niveau": "3e",
+        "matiere": "Géométrie",
+        "titre": "Trigonométrie",
+        "id": "3e-2025-2026-trigonometrie",
+        "desc": "Le lien entre angles et longueurs dans le triangle rectangle : Cosinus, Sinus et Tangente.",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/3e/02 Trigonométrie/Séquence 2 _ Trigonométrie.pdf"
+            },
+            {
+                "nom": "Exercices",
+                "url": "cours/25-26/3e/02 Trigonométrie/Exercices - Trigonométrie.pdf"
+            },
+            {
+                "nom": "Correction",
+                "url": "cours/25-26/3e/02 Trigonométrie/Correction - Trigonométrie.pdf"
+            }
         ],
-        tags: ["cosinus", "sinus", "tangente", "sohcahtoa", "cahsohtoa", "angle", "triangle rectangle", "hypotenuse", "brevet"]
+        "tags": [
+            "cosinus",
+            "sinus",
+            "tangente",
+            "sohcahtoa",
+            "cahsohtoa",
+            "angle",
+            "triangle rectangle",
+            "hypotenuse",
+            "brevet"
+        ]
     },
     {
-        annee: "2025-2026",
-        date: "2025-11-12",
-        niveau: "3e",
-        matiere: "Gestion de données",
-        titre: "Probabilités",
-        id: "3e-2025-2026-probabilites",
-        desc: "Apprendre à mesurer le hasard, utiliser des tableaux de probabilités et comprendre les fréquences.",
-        fichiers: [
-            { nom: "Leçon", url: "cours/25-26/3e/03 Probabilités/Séquence 3 _ Probabilités.pdf"},
-			{ nom: "Exercices", url: "cours/25-26/3e/03 Probabilités/Exercices - Probabilités.pdf"},
-            { nom: "Correction", url: "cours/25-26/3e/03 Probabilités/Correction - Probabilités.pdf"}
+        "annee": "2025-2026",
+        "date": "2025-11-12",
+        "niveau": "3e",
+        "matiere": "Gestion de données",
+        "titre": "Probabilités",
+        "id": "3e-2025-2026-probabilites",
+        "desc": "Apprendre à mesurer le hasard, utiliser des tableaux de probabilités et comprendre les fréquences.",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/3e/03 Probabilités/Séquence 3 _ Probabilités.pdf"
+            },
+            {
+                "nom": "Exercices",
+                "url": "cours/25-26/3e/03 Probabilités/Exercices - Probabilités.pdf"
+            },
+            {
+                "nom": "Correction",
+                "url": "cours/25-26/3e/03 Probabilités/Correction - Probabilités.pdf"
+            }
         ],
-        tags: ["probabilité", "événement", "fréquence", "tableau", "brevet", "hasard", "chance", "expérience aléatoire"]
+        "tags": [
+            "probabilité",
+            "événement",
+            "fréquence",
+            "tableau",
+            "brevet",
+            "hasard",
+            "chance",
+            "expérience aléatoire"
+        ]
     },
-    { 
-        annee: "2025-2026",
-        date: "2026-01-15",
-        niveau: "3e",
-        matiere: "Nombres et calcul", 
-        titre: "Calcul littéral",
-        id: "3e-2025-2026-calcul-litteral",
-        desc: "Développer, factoriser : changer l'apparence d'une expression par le calcul pour correspondre à nos besoins.", 
-        fichiers: [
-            { nom: "Leçon", url: "cours/25-26/3e/04 Calcul littéral/Séquence 4 _ Calcul littéral.pdf"},
-			{ nom: "Exercices", url: "cours/25-26/3e/04 Calcul littéral/Exercices - Calcul littéral.pdf"},
-			{ nom: "Correction", url: "cours/25-26/3e/04 Calcul littéral/Correction - Calcul littéral.pdf"}
+    {
+        "annee": "2025-2026",
+        "date": "2026-01-15",
+        "niveau": "3e",
+        "matiere": "Nombres et calcul",
+        "titre": "Calcul littéral",
+        "id": "3e-2025-2026-calcul-litteral",
+        "desc": "Développer, factoriser : changer l'apparence d'une expression par le calcul pour correspondre à nos besoins.",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/3e/04 Calcul littéral/Séquence 4 _ Calcul littéral.pdf"
+            },
+            {
+                "nom": "Exercices",
+                "url": "cours/25-26/3e/04 Calcul littéral/Exercices - Calcul littéral.pdf"
+            },
+            {
+                "nom": "Correction",
+                "url": "cours/25-26/3e/04 Calcul littéral/Correction - Calcul littéral.pdf"
+            }
         ],
-        tags: ["developper", "factoriser", "identite remarquable", "distributivite", "equation", "x", "brevet"]
+        "tags": [
+            "developper",
+            "factoriser",
+            "identite remarquable",
+            "distributivite",
+            "equation",
+            "x",
+            "brevet"
+        ]
     },
-    { 
-        annee: "2025-2026",
-        date: "2026-02-20",
-        niveau: "3e",
-        matiere: "Géométrie", 
-        titre: "Théorème de Thalès", 
-        id: "3e-2025-2026-theoreme-de-thales",
-        desc: "Utiliser l'égalité des rapports pour calculer des longueurs ou prouver un parallélisme.", 
-        fichiers: [
-            { nom: "Leçon", url: "cours/25-26/3e/05 Thalès/Séquence 5 _ Thalès.pdf"},
-			{ nom: "Exercices", url: "cours/25-26/3e/05 Thalès/Exercices - Thalès.pdf"},
-			{ nom: "Correction", url: "cours/25-26/3e/05 Thalès/Correction - Thalès.pdf"}
+    {
+        "annee": "2025-2026",
+        "date": "2026-02-20",
+        "niveau": "3e",
+        "matiere": "Géométrie",
+        "titre": "Théorème de Thalès",
+        "id": "3e-2025-2026-theoreme-de-thales",
+        "desc": "Utiliser l'égalité des rapports pour calculer des longueurs ou prouver un parallélisme.",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/3e/05 Thalès/Séquence 5 _ Thalès.pdf"
+            },
+            {
+                "nom": "Exercices",
+                "url": "cours/25-26/3e/05 Thalès/Exercices - Thalès.pdf"
+            },
+            {
+                "nom": "Correction",
+                "url": "cours/25-26/3e/05 Thalès/Correction - Thalès.pdf"
+            }
         ],
-        tags: ["thales", "agrandissement", "reduction", "parallele", "triangle", "rapports", "papillon", "brevet"]
+        "tags": [
+            "thales",
+            "agrandissement",
+            "reduction",
+            "parallele",
+            "triangle",
+            "rapports",
+            "papillon",
+            "brevet"
+        ]
     },
-    { 
-        annee: "2025-2026",
-        date: "2026-03-25",
-        niveau: "3e",
-        matiere: "Fonctions", 
-        titre: "Fonctions affines et linéaires",
-        id: "3e-2025-2026-fonctions-affines-et-lineaires", 
-        desc: "Étudier les droites : celles qui passent par l'origine et celles qui ont une ordonnée à l'origine.", 
-        fichiers: [
-            { nom: "Leçon", url: "cours/25-26/3e/06 Fonctions affines et linéaires/Séquence 6 _ Fonctions affines et linéaires.pdf"},
-			{ nom: "Exercices", url: "cours/25-26/3e/06 Fonctions affines et linéaires/Exercices - Fonctions affines et linéaires.pdf"},
-			{ nom: "Correction", url: "cours/25-26/3e/06 Fonctions affines et linéaires/Correction - Fonctions affines et linéaires.pdf"}
+    {
+        "annee": "2025-2026",
+        "date": "2026-03-25",
+        "niveau": "3e",
+        "matiere": "Fonctions",
+        "titre": "Fonctions affines et linéaires",
+        "id": "3e-2025-2026-fonctions-affines-et-lineaires",
+        "desc": "Étudier les droites : celles qui passent par l'origine et celles qui ont une ordonnée à l'origine.",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/3e/06 Fonctions affines et linéaires/Séquence 6 _ Fonctions affines et linéaires.pdf"
+            },
+            {
+                "nom": "Exercices",
+                "url": "cours/25-26/3e/06 Fonctions affines et linéaires/Exercices - Fonctions affines et linéaires.pdf"
+            },
+            {
+                "nom": "Correction",
+                "url": "cours/25-26/3e/06 Fonctions affines et linéaires/Correction - Fonctions affines et linéaires.pdf"
+            }
         ],
-        tags: ["proportionnalite", "droite", "coefficient directeur", "ordonnee a l'origine", "lineaire", "affine", "brevet"]
+        "tags": [
+            "proportionnalite",
+            "droite",
+            "coefficient directeur",
+            "ordonnee a l'origine",
+            "lineaire",
+            "affine",
+            "brevet"
+        ]
     },
-    { 
-        annee: "2025-2026",
-        date: "2026-04-30",
-        niveau: "3e",
-        matiere: "Gestion de données", 
-        titre: "Statistiques",
-        id: "3e-2025-2026-statistiques",
-        desc: "Analyser des séries de données : moyenne, médiane, étendue et interprétation de graphiques.", 
-        fichiers: [
-            { nom: "Leçon", url: "cours/25-26/3e/07 Statistiques/Séquence 7 _ Statistiques.pdf"},
-			{ nom: "Exercices", url: "cours/25-26/3e/07 Statistiques/Exercices - Statistiques.pdf"},
-			{ nom: "Correction", url: "cours/25-26/3e/07 Statistiques/Correction - Statistiques.pdf"}
+    {
+        "annee": "2025-2026",
+        "date": "2026-04-30",
+        "niveau": "3e",
+        "matiere": "Gestion de données",
+        "titre": "Statistiques",
+        "id": "3e-2025-2026-statistiques",
+        "desc": "Analyser des séries de données : moyenne, médiane, étendue et interprétation de graphiques.",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/3e/07 Statistiques/Séquence 7 _ Statistiques.pdf"
+            },
+            {
+                "nom": "Exercices",
+                "url": "cours/25-26/3e/07 Statistiques/Exercices - Statistiques.pdf"
+            },
+            {
+                "nom": "Correction",
+                "url": "cours/25-26/3e/07 Statistiques/Correction - Statistiques.pdf"
+            }
         ],
-        tags: ["moyenne", "mediane", "etendue", "serie", "effectif", "frequence", "indicateurs", "brevet"]
+        "tags": [
+            "moyenne",
+            "mediane",
+            "etendue",
+            "serie",
+            "effectif",
+            "frequence",
+            "indicateurs",
+            "brevet"
+        ]
+    },
+    {
+        "id": "3e-2025-2026-calcul-litteral-partie-2",
+        "annee": "2025-2026",
+        "date": "2026-03-15",
+        "niveau": "3e",
+        "matiere": "Nombres et calcul",
+        "titre": "Calcul littéral (partie 2)",
+        "desc": "Identités remarquables, équations produit nul.",
+        "tags": [
+            "factoriser",
+            "développer",
+            "équations",
+            "factorisation",
+            "développement"
+        ],
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/3e/10 Calcul littéral 2/Séquence 9 _ Transformations.pdf"
+            }
+        ]
+    },
+    {
+        "id": "3e-2025-2026-transformations-du-plan",
+        "annee": "2025-2026",
+        "date": "2026-03-14",
+        "niveau": "3e",
+        "matiere": "Géométrie",
+        "titre": "Transformations du plan",
+        "desc": "Les différentes transformations que l'on peut appliquer à une figure : symétries, translation, rotation et homothétie. ",
+        "tags": [
+            "symétrie",
+            "axiale",
+            "centrale",
+            "rotation",
+            "translation",
+            "homothétie",
+            "vecteur",
+            "agrandissement",
+            "réduction"
+        ],
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/3e/09 Transformations/Séquence 9 _ Transformations.pdf"
+            }
+        ]
+    },
+    {
+        "id": "3e-2025-2026-arithmetique",
+        "annee": "2025-2026",
+        "date": "2026-03-13",
+        "niveau": "3e",
+        "matiere": "Nombres et calcul",
+        "titre": "Arithmétique",
+        "desc": "Maîtriser les nombres premiers pour résoudre des problèmes complexes.",
+        "tags": [
+            "premier",
+            "nombre",
+            "entier",
+            "divisibilité",
+            "diviseur",
+            "fraction",
+            "simplifier",
+            "simplification"
+        ],
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "documents/3e/Séquence 8 _ Arithmétique.pdf"
+            },
+            {
+                "nom": "Exercices",
+                "url": "documents/3e/Séquence 8 _ Arithmétique - Exercices.pdf"
+            },
+            {
+                "nom": "Correction",
+                "url": "documents/3e/Séquence 8 _ Arithmétique - Correction.pdf"
+            }
+        ]
     }
 ];
