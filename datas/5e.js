13921e9dd33881d09a9ed0c668729cd5d559306f
Faellatv <faellatv@gmail.com>
2026-03-15 15:01:34 +0100

    5e
    Séquence Calcul littéral

--- datas/5e.js
+++ datas/5e.js
@@ -1,145 +1,295 @@
 var data5e = [
-    { 
-        annee: "2025-2026",
-		date: "2025-09-01",
-		niveau: "5e",
-        matiere: "Nombres et calcul", 
-        titre: "Priorité opératoire", 
-        id: "5e-2025-2026-priorite-operatoire",
-        desc: "Par quelle opération commencer ?", 
-        fichiers: [
-            { nom: "Leçon", url: "cours/25-26/5e/Séquence 1 _ Priorité opératoire.pdf"}
+    {
+        "annee": "2025-2026",
+        "date": "2025-09-01",
+        "niveau": "5e",
+        "matiere": "Nombres et calcul",
+        "titre": "Priorité opératoire",
+        "id": "5e-2025-2026-priorite-operatoire",
+        "desc": "Par quelle opération commencer ?",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/5e/Séquence 1 _ Priorité opératoire.pdf"
+            }
         ],
-        tags: ["calcul", "parenthèses", "priorités", "opérations", "multiplication", "addition", "soustraction", "division", "quotient"]
+        "tags": [
+            "calcul",
+            "parenthèses",
+            "priorités",
+            "opérations",
+            "multiplication",
+            "addition",
+            "soustraction",
+            "division",
+            "quotient"
+        ]
     },
-	{ 
-        annee: "2025-2026",
-		date: "2025-09-02",
-		niveau: "5e",
-        matiere: "Géométrie", 
-        titre: "Inégalité triangulaire", 
-        id: "5e-2025-2026-inegalite-triangulaire",
-        desc: "Le plus court chemin est toujours tout droit !", 
-        fichiers: [
-            { nom: "Leçon", url: "cours/25-26/5e/Séquence 2 _ Inégalité triangulaire.pdf"}
+    {
+        "annee": "2025-2026",
+        "date": "2025-09-02",
+        "niveau": "5e",
+        "matiere": "Géométrie",
+        "titre": "Inégalité triangulaire",
+        "id": "5e-2025-2026-inegalite-triangulaire",
+        "desc": "Le plus court chemin est toujours tout droit !",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/5e/Séquence 2 _ Inégalité triangulaire.pdf"
+            }
         ],
-        tags: ["triangle", "construction", "longueurs", "compas", "constructibilité", "aligné"]
+        "tags": [
+            "triangle",
+            "construction",
+            "longueurs",
+            "compas",
+            "constructibilité",
+            "aligné"
+        ]
     },
-	{ 
-        annee: "2025-2026",
-		date: "2025-09-03",
-		niveau: "5e",
-        matiere: "Nombres et calcul", 
-        titre: "Nombres relatifs", 
-        id: "5e-2025-2026-nombres-relatifs",
-        desc: "Un nombre relatif, c'est quoi ?", 
-        fichiers: [
-            { nom: "Leçon", url: "cours/25-26/5e/Séquence 3 _ Nombres relatifs.pdf"}
+    {
+        "annee": "2025-2026",
+        "date": "2025-09-03",
+        "niveau": "5e",
+        "matiere": "Nombres et calcul",
+        "titre": "Nombres relatifs",
+        "id": "5e-2025-2026-nombres-relatifs",
+        "desc": "Un nombre relatif, c'est quoi ?",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/5e/Séquence 3 _ Nombres relatifs.pdf"
+            }
         ],
-		tags: ["négatif", "positif", "zéro", "température", "opposé", "abscisse", "repérage", "ordonnée"]
+        "tags": [
+            "négatif",
+            "positif",
+            "zéro",
+            "température",
+            "opposé",
+            "abscisse",
+            "repérage",
+            "ordonnée"
+        ]
     },
-	{ 
-        annee: "2025-2026",
-		date: "2025-09-04",
-		niveau: "5e",
-        matiere: "Géométrie", 
-        titre: "Somme des angles dans un triangle", 
-        id: "5e-2025-2026-somme-des-angles-dans-un-triangle",
-        desc: "La règle des 180° !", 
-        fichiers: [
-            { nom: "Leçon", url: "cours/25-26/5e/Séquence 4 _ Somme des angles dans un triangle.pdf"}
+    {
+        "annee": "2025-2026",
+        "date": "2025-09-04",
+        "niveau": "5e",
+        "matiere": "Géométrie",
+        "titre": "Somme des angles dans un triangle",
+        "id": "5e-2025-2026-somme-des-angles-dans-un-triangle",
+        "desc": "La règle des 180° !",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/5e/Séquence 4 _ Somme des angles dans un triangle.pdf"
+            }
         ],
-		tags: ["180", "isocèle", "équilatéral", "rectangle", "triangle", "constructibilité"]
+        "tags": [
+            "180",
+            "isocèle",
+            "équilatéral",
+            "rectangle",
+            "triangle",
+            "constructibilité"
+        ]
     },
-	{ 
-        annee: "2025-2026",
-		date: "2025-09-05",
-		niveau: "5e",
-        matiere: "Gestion de données", 
-        titre: "Proportionnalité", 
-        id: "5e-2025-2026-proportionnalite",
-        desc: "Si j'en achète 3 fois plus, je paie ... ?", 
-        fichiers: [
-            { nom: "Leçon", url: "cours/25-26/5e/Séquence 5 _ Proportionnalité.pdf"}
+    {
+        "annee": "2025-2026",
+        "date": "2025-09-05",
+        "niveau": "5e",
+        "matiere": "Gestion de données",
+        "titre": "Proportionnalité",
+        "id": "5e-2025-2026-proportionnalite",
+        "desc": "Si j'en achète 3 fois plus, je paie ... ?",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/5e/Séquence 5 _ Proportionnalité.pdf"
+            }
         ],
-        tags: ["ratio", "produit en croix", "tableau", "coefficient", "quatrième", "échelle", "pourcentage"]
+        "tags": [
+            "ratio",
+            "produit en croix",
+            "tableau",
+            "coefficient",
+            "quatrième",
+            "échelle",
+            "pourcentage"
+        ]
     },
-	{ 
-        annee: "2025-2026",
-		date: "2025-09-06",
-		niveau: "5e",
-        matiere: "Nombres et calcul", 
-        titre: "Fractions", 
-        id: "5e-2025-2026-fractions",
-        desc: "Manipulation de fractions, pourcentages.", 
-        fichiers: [
-            { nom: "Leçon", url: "cours/25-26/5e/Séquence 6 _ Fractions.pdf"}
+    {
+        "annee": "2025-2026",
+        "date": "2025-09-06",
+        "niveau": "5e",
+        "matiere": "Nombres et calcul",
+        "titre": "Fractions",
+        "id": "5e-2025-2026-fractions",
+        "desc": "Manipulation de fractions, pourcentages.",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/5e/Séquence 6 _ Fractions.pdf"
+            }
         ],
-		tags: ["égal", "simplifier", "quantité", "numérateur", "dénominateur", "prtoportion"]
+        "tags": [
+            "égal",
+            "simplifier",
+            "quantité",
+            "numérateur",
+            "dénominateur",
+            "prtoportion"
+        ]
     },
-	{ 
-        annee: "2025-2026",
-		date: "2025-09-07",
-		niveau: "5e",
-        matiere: "Géométrie", 
-        titre: "Angles et parallélisme", 
-        id: "5e-2025-2026-angles-et-parallelisme",
-        desc: "Angles adjacents, opposés par le sommet, ...", 
-        fichiers: [
-            { nom: "Leçon", url: "cours/25-26/5e/Séquence 7 _ Angles et parallélisme.pdf"}
+    {
+        "annee": "2025-2026",
+        "date": "2025-09-07",
+        "niveau": "5e",
+        "matiere": "Géométrie",
+        "titre": "Angles et parallélisme",
+        "id": "5e-2025-2026-angles-et-parallelisme",
+        "desc": "Angles adjacents, opposés par le sommet, ...",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/5e/Séquence 7 _ Angles et parallélisme.pdf"
+            }
         ],
-        tags: ["droites", "parallèles", "sécante", "alternes-internes", "correspondants", "sommet", "opposé", "adjacent", "complémentaire", "supplémentaire"]
+        "tags": [
+            "droites",
+            "parallèles",
+            "sécante",
+            "alternes-internes",
+            "correspondants",
+            "sommet",
+            "opposé",
+            "adjacent",
+            "complémentaire",
+            "supplémentaire"
+        ]
     },
-	{ 
-        annee: "2025-2026",
-		date: "2025-09-08",
-		niveau: "5e",
-        matiere: "Nombres et calcul", 
-        titre: "Addition et soustraction de relatifs", 
-        id: "5e-2025-2026-addition-et-soustraction-de-relatifs",
-        desc: "Attention au signe !", 
-        fichiers: [
-            { nom: "Leçon", url: "cours/25-26/5e/Séquence 8 _ Additionner et soustraire des nombres relatifs.pdf"}
+    {
+        "annee": "2025-2026",
+        "date": "2025-09-08",
+        "niveau": "5e",
+        "matiere": "Nombres et calcul",
+        "titre": "Addition et soustraction de relatifs",
+        "id": "5e-2025-2026-addition-et-soustraction-de-relatifs",
+        "desc": "Attention au signe !",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/5e/Séquence 8 _ Additionner et soustraire des nombres relatifs.pdf"
+            }
         ],
-        tags: ["calcul", "signe", "plus", "moins", "somme", "différence", "parentheses"]
+        "tags": [
+            "calcul",
+            "signe",
+            "plus",
+            "moins",
+            "somme",
+            "différence",
+            "parentheses"
+        ]
     },
-	{ 
-        annee: "2025-2026",
-		date: "2025-09-09",
-		niveau: "5e",
-        matiere: "Géométrie", 
-        titre: "Droites particulières dans un triangle", 
-        id: "5e-2025-2026-droites-particulieres-dans-un-triangle",
-        desc: "Médiatrices et hauteurs dans un triangle", 
-        fichiers: [
-            { nom: "Leçon", url: "cours/25-26/5e/Séquence 9 _ Droites particulières dans un triangle.pdf"}
+    {
+        "annee": "2025-2026",
+        "date": "2025-09-09",
+        "niveau": "5e",
+        "matiere": "Géométrie",
+        "titre": "Droites particulières dans un triangle",
+        "id": "5e-2025-2026-droites-particulieres-dans-un-triangle",
+        "desc": "Médiatrices et hauteurs dans un triangle",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/5e/Séquence 9 _ Droites particulières dans un triangle.pdf"
+            }
         ],
-        tags: ["hauteur", "médiatrice", "sommet", "angle droit", "orthocentre", "compas", "circonscrit"]
+        "tags": [
+            "hauteur",
+            "médiatrice",
+            "sommet",
+            "angle droit",
+            "orthocentre",
+            "compas",
+            "circonscrit"
+        ]
     },
-	{ 
-        annee: "2025-2026",
-		date: "2025-09-10",
-		niveau: "5e",
-        matiere: "Nombres et calcul", 
-        titre: "Addition et soustraction de fractions", 
-        id: "5e-2025-2026-addition-et-soustraction-de-fractions",
-        desc: "Le dénominateur doit être commun !", 
-        fichiers: [
-            { nom: "Leçon", url: "cours/25-26/5e/Séquence 10 _ Addition et soustraction de fractions.pdf"}
+    {
+        "annee": "2025-2026",
+        "date": "2025-09-10",
+        "niveau": "5e",
+        "matiere": "Nombres et calcul",
+        "titre": "Addition et soustraction de fractions",
+        "id": "5e-2025-2026-addition-et-soustraction-de-fractions",
+        "desc": "Le dénominateur doit être commun !",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/5e/Séquence 10 _ Addition et soustraction de fractions.pdf"
+            }
         ],
-        tags: ["fraction", "numérateur", "dénominateur", "partage", "calcul", "proportion"]
+        "tags": [
+            "fraction",
+            "numérateur",
+            "dénominateur",
+            "partage",
+            "calcul",
+            "proportion"
+        ]
     },
-	{ 
-        annee: "2025-2026",
-		date: "2025-09-11",
-		niveau: "5e",
-        matiere: "Géométrie", 
-        titre: "Symétrie axiale et centrale", 
-        id: "5e-2025-2026-symetrie-axiale-et-centrale",
-        desc: "Symétrie miroir ou demi-tour ?", 
-        fichiers: [
-            { nom: "Leçon", url: "cours/25-26/5e/Séquence 11 _ Symétrie axiale et centrale.pdf"}
+    {
+        "annee": "2025-2026",
+        "date": "2025-09-11",
+        "niveau": "5e",
+        "matiere": "Géométrie",
+        "titre": "Symétrie axiale et centrale",
+        "id": "5e-2025-2026-symetrie-axiale-et-centrale",
+        "desc": "Symétrie miroir ou demi-tour ?",
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/5e/Séquence 11 _ Symétrie axiale et centrale.pdf"
+            }
         ],
-        tags: ["transformation", "demi-tour", "centre", "construction", "parallélisme", "miroir", "figure"]
+        "tags": [
+            "transformation",
+            "demi-tour",
+            "centre",
+            "construction",
+            "parallélisme",
+            "miroir",
+            "figure"
+        ]
+    },
+    {
+        "id": "5e-2025-2026-calcul-litteral",
+        "annee": "2025-2026",
+        "date": "2026-03-15",
+        "niveau": "5e",
+        "matiere": "Nombres et calcul",
+        "titre": "Calcul littéral",
+        "desc": "Qu'est-ce qu'une expression littérale ? Comment calculer ? Développer et simplifier.",
+        "tags": [
+            "développement",
+            "développer",
+            "simplifier",
+            "littéral",
+            "calcul",
+            "x",
+            "lettre",
+            "égalité",
+            "tester",
+            "programme"
+        ],
+        "fichiers": [
+            {
+                "nom": "Leçon",
+                "url": "cours/25-26/5e/Séquence 12 _ Calcul littéral.pdf"
+            }
+        ]
     }
 ];
